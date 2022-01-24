8d5d53bff0857dd4ab79b4106b15248d92f5d026
Carlos9852 <87607986+Carlos9852@users.noreply.github.com>
2022-01-24 20:58:17 -0300

    Add files via upload

---
+++ TestMyTheme.ino
@@ -0,0 +1,26 @@
+//A comment
+/* Another comment */
+
+int  led = 0x0d; //hexadecimal
+unsigned long  Now = 0; //decimal
+float i = 1.0; //decimal
+char c = 'd'; //char
+String s = "Hello, World!"; //string
+boolean b = true; //boolean
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+
+  if(b == true){
+    Serial.println(s);
+  }
+}
+
+void loop() {
+  if(((millis() - Now) > 1000)){
+    Now = millis();
+    Serial.println(millis() / 1000);
+    digitalWrite(led, !digitalRead(led));   
+  }
+}
